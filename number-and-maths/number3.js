58cca0a4899f19120cd2fdb4ef9d244676d85cda
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-02-19 14:26:27 +0000

    number commit 02

---
+++ number-and-maths/number3.js
@@ -0,0 +1,22 @@
+// +++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++
+
+// by default imported in every program of js
+
+console.log(Math)
+console.log(Math.abs(-4)); // 4
+console.log(Math.abs(4)); // 4
+console.log(Math.round(4.3)); // 4
+console.log(Math.round(4.6));// 5
+console.log(Math.round(4.5)); // 5
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.floor(4.9)); // 4
+console.log(Math.sqrt(5));
+
+console.log(Math.min([1,2,3])); // NaN
+console.log(Math.min(1,2,3));
+console.log(Math.max(1,2,3));
+console.log(Math.pow(3,2));
+
+
+
+
---
+++ number-and-maths/number4.js
@@ -0,0 +1,9 @@
+console.log(Math.random()); // 0(inclusive) to 1(exclusive)
+console.log(Math.random() * 10);
+
+const min = 10;
+const max = 20;
+
+
+const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
