0b2d06aabc63f1b57b59a9cac7ecf1091c5712df
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-03-01 13:46:18 +0000

    loop-commit-1

---
+++ for-loop-with-break-and-continue/loop1.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 10; i++) {
+    const element = i
+    console.log(element); // shortcut for multicursors -> repeteadly press cntrl + d
+     if (element == 5)
+     {
+        console.log("5 is best number"); 
+     }
+    
+}
+//console.log(element); // ReferenceError: element is not defined
