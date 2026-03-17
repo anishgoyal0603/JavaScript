68ae04887a9699b6ad915b9f9fa5c5674a9487e1
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-03-17 17:04:52 +0000

    call-commit

---
+++ call-and-this-in-javascript/call4.js
@@ -0,0 +1,17 @@
+//valid
+function setUserName(username)
+{
+    console.log(this)
+    this.username = username
+};
+
+
+function createUser(username , email , password)
+{
+    setUserName.call(this,username)
+    this.email = email
+    this.password = password
+};
+
+const chai = new createUser('chai','chai@fb.com','123')
+console.log(chai)
