0db2a13114e4a7e9ff63a78c7c746d96a7eedb72
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-03-20 17:58:34 +0000

    class-commit

---
+++ class-constructor-and-static/class4.js
@@ -0,0 +1,24 @@
+
+
+
+function User(username,email,password) 
+{
+    this.username = username
+    this.email = email
+    this.password = password
+
+    User.prototype.encryptPassword = function() {
+    return `${this.password}abc`
+}
+
+User.prototype.changeUserName = function(){
+     return `${this.username.toUpperCase()}`
+}
+
+}
+
+
+const tea = new User("tea","tea@gmail.com","123")
+
+console.log(tea.encryptPassword())
+console.log(tea.changeUserName())
