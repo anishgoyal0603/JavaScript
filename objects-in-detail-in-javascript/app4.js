7bcba6d5483be2bbff66cc12f36bbea63d52af43
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-03-24 17:40:29 +0000

    objects-commit

---
+++ objects-in-detail-in-javascript/app4.js
@@ -0,0 +1,22 @@
+const chai = {
+    name : 'ginger chai',
+    price : 250,
+    isAvailable : true,
+    orderChai : function (){
+        console.log("dhari wala")
+    }
+}
+console.log(chai)
+
+Object.defineProperty(chai,'name',{
+    writable : false,
+    enumerable : false
+})
+
+for (let [key,value] of Object.entries(chai))
+{
+    if (typeof value !== 'function')
+    {
+    console.log(`${key} : ${value}`);
+    }
+}
